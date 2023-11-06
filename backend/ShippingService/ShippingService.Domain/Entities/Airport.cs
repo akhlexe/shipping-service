67313597e33e397693cc470c8b4e769832e4856b
Exequiel <exe.pine@gmail.com>
2023-11-06 20:04:34 -0300

    Agrego entidad y context inicial

---
+++ backend/ShippingService/ShippingService.Domain/Entities/Airport.cs
@@ -0,0 +1,21 @@
+﻿namespace ShippingService.Domain.Entities
+{
+    public class Airport
+    {
+        public int Id { get; set; }
+
+        public string IATA_Code { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public string Continent { get; set; }
+
+        public string CountryCode { get; set; }
+    }
+}
---
+++ backend/ShippingService/ShippingService.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShippingService.Domain.Entities;
+
+namespace ShippingService.Infrastructure.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Airport> Airports { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
