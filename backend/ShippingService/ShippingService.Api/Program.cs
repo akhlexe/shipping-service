deb973c208afefd1aac15db259a5a8e788601f0a
Exequiel <exe.pine@gmail.com>
2023-11-06 22:21:48 -0300

    Agrego database infra, migraciones y docker compose para levantar DB

--- backend/ShippingService/ShippingService.Api/Program.cs
+++ backend/ShippingService/ShippingService.Api/Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ShippingService.Api.Utilities;
+using ShippingService.Infrastructure.Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+PrepDB.Migrate(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ backend/ShippingService/ShippingService.Api/Utilities/PrepDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShippingService.Infrastructure.Persistence.Data;
+
+namespace ShippingService.Api.Utilities
+{
+    public class PrepDB
+    {
+        public static void Migrate(IApplicationBuilder builder)
+        {
+            using var scope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.Migrate();
+        }
+    }
+}
